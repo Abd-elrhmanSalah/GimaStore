eedb4f2daeece9e162cbb72a830a20d1ef29cd9a
Abdelrahman_Salah <abdelrahman_salah>
2023-05-24 17:21:24 +0300

    --First Initialization

---
+++ src/main/java/com/gima/gimastore/GimaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gima.gimastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gima/gimastore/GimaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gima.gimastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
