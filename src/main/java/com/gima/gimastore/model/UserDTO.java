432196b49b35032de9897b1c8279e253729d7132
Abdelrahman_Salah <abdelrahman_salah>
2023-05-27 01:14:25 +0300

    --clear lombo

--- src/main/java/com/gima/gimastore/model/UserDTO.java
+++ src/main/java/com/gima/gimastore/model/UserDTO.java
@@ -2,14 +2,11 @@ package com.gima.gimastore.model;
 
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.io.Serializable;
 
-@Setter
-@Getter
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO implements Serializable {
@@ -23,6 +20,53 @@ public class UserDTO implements Serializable {
     private String firstName;
     @NotNull
     private String lastName;
-
     private Long roleId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
 }
