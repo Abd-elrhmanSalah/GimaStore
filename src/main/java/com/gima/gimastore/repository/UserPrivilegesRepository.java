02b4de66aed57a93e396d6b572e6b474c77abdd5
Abdelrahman <abdo.salah884@gmail.com>
2023-07-15 02:18:39 +0300

    update getAllUsers to get Privileges

--- src/main/java/com/gima/gimastore/repository/UserPrivilegesRepository.java
+++ src/main/java/com/gima/gimastore/repository/UserPrivilegesRepository.java
@@ -5,7 +5,10 @@ import com.gima.gimastore.entity.UserPrivileges;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPrivilegesRepository extends JpaRepository<UserPrivileges, Long> {
-    UserPrivileges findByUser(User use);
+//    UserPrivileges findByUser(User use);
+    Optional<UserPrivileges> findByUser(User user);
 }
